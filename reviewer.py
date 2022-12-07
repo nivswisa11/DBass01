f322f11a92578ba10bf960fd4e0be4098dea7448
guyreuveni33 <guyhaim55@gmail.com>
2022-12-07 12:49:05 +0200

    step 1-3 include third try

--- reviewer.py
+++ reviewer.py
@@ -17,20 +17,22 @@ cursor = cnx.cursor()
 cursor.execute("""
     CREATE TABLE if not exists rating (
       film_id smallint,
-      review_id INT,
-      rating DECIMAL(2,1)
+      reviewer_id INT,
+      rating DECIMAL(2,1),
+      FOREIGN KEY (film_id) REFERENCES film(film_id)
+      FOREIGN KEY (reviewer_id) REFERENCES reviewer(reviewer_id)
       CHECK (REGEXP_LIKE(film_id,'^([0-9]+)$')),
-      CHECK (REGEXP_LIKE(review_id,'^([0-9]+)$')),
-      CHECK (REGEXP_LIKE(rating,'^([0-9]\(.[0-9])?)$')),
+      CHECK (REGEXP_LIKE(reviewer_id,'^([0-9]+)$')),
+      CHECK (REGEXP_LIKE(rating,'^([0-9])(\\.[0-9])?)$')),
     );
 """)
 # Create the table reviewer if not exists
 cursor.execute("""
     CREATE TABLE if not exists reviewer (
-      review_id INT NOT NULL PRIMARY KEY,
+      reviewer_id INT NOT NULL PRIMARY KEY,
       first_name VARCHAR(45),
       last_name VARCHAR(45),
-      CHECK (REGEXP_LIKE(review_id,'^([0-9]+)$')),
+      CHECK (REGEXP_LIKE(reviewer_id,'^([0-9]+)$')),
       CHECK (REGEXP_LIKE(first_name,'^([A-Za-z]+( [A-Za-z]+)?)$')),
       CHECK (REGEXP_LIKE(last_name,'^([A-Za-z]+( [A-Za-z]+)?)$'))
     );
@@ -39,13 +41,13 @@ while True:
     idInput = input("Insert your ID please:\n")
     tableOne = cursor.execute(("""SELECT *
                    FROM reviewer
-                    WHERE review_id=%s
+                    WHERE reviewer_id=%s
     """), (idInput,))
     cursor.fetchall()
     firstName = input("Insert your first name please:\n")
     lastName = input("Insert your last name please:\n")
     try:
-        cursor.execute('INSERT INTO reviewer (review_id, first_name,last_name) VALUES (%s, %s,%s)',
+        cursor.execute('INSERT INTO reviewer (reviewer_id, first_name,last_name) VALUES (%s, %s,%s)',
                        (idInput, firstName, lastName))
         cnx.commit()
         break
@@ -58,22 +60,26 @@ else:
 cursor.fetchall()
 while True:
     filmName = input("Please enter a film name:")
-    rating = input("Please enter a rating for the film:")
     tableTwo = cursor.execute(("""SELECT *
                                         FROM film 
                                         WHERE title=%s
             """), (filmName,))
-    cursor.fetchall()
-    filmID=cursor.execute(("""SELECT film.film_id
-                                        FROM film 
-                                        WHERE title=%s
-            """), (filmName,))
-    try:
-        cursor.execute('INSERT INTO rating (film_id, review_id,rating) VALUES (%s, %s,%s)',
-                       (,idInput, rating))
-        cnx.commit()
-        break
-    except:
+    if not tableTwo:
         continue
-cursor.fetchall()
+    filmID = cursor.execute(("""SELECT film.film_id
+                                FROM film 
+                                WHERE title=%s
+    """), (filmName,))
+    if len(tableTwo)==1:
+        rating = input("Please enter a rating for the film:")
+        try:
+            cursor.execute('INSERT INTO rating (film_id,reviewer_id,rating) VALUES (%s, %s,%s)',
+                       (filmID,idInput, rating))
+            cnx.commit()
+            break
+        except:
+            continue
+    cursor.fetchall()
+
+
 
