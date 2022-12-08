74cfd763426195281bc5ccbeb9ed772527c14321
guyreuveni33 <guyhaim55@gmail.com>
2022-12-08 13:02:17 +0200

    step 1-3 include fourth try

--- reviewer.py
+++ reviewer.py
@@ -19,11 +19,10 @@ cursor.execute("""
       film_id smallint,
       reviewer_id INT,
       rating DECIMAL(2,1),
-      FOREIGN KEY (film_id) REFERENCES film(film_id)
-      FOREIGN KEY (reviewer_id) REFERENCES reviewer(reviewer_id)
-      CHECK (REGEXP_LIKE(film_id,'^([0-9]+)$')),
-      CHECK (REGEXP_LIKE(reviewer_id,'^([0-9]+)$')),
-      CHECK (REGEXP_LIKE(rating,'^([0-9])(\\.[0-9])?)$')),
+      CHECK (rating>=0 AND rating<=9.9),
+      PRIMARY KEY(film_id,reviewer_id),
+      FOREIGN KEY (film_id) REFERENCES film(film_id) ON UPDATE CASCADE ON DELETE CASCADE,
+      FOREIGN KEY (reviewer_id) REFERENCES reviewer(reviewer_id) ON UPDATE CASCADE ON DELETE CASCADE
     );
 """)
 # Create the table reviewer if not exists
@@ -39,11 +38,11 @@ cursor.execute("""
 """)
 while True:
     idInput = input("Insert your ID please:\n")
-    tableOne = cursor.execute(("""SELECT *
+    cursor.execute(("""SELECT *
                    FROM reviewer
                     WHERE reviewer_id=%s
     """), (idInput,))
-    cursor.fetchall()
+    tableOne=cursor.fetchall()
     firstName = input("Insert your first name please:\n")
     lastName = input("Insert your last name please:\n")
     try:
@@ -53,6 +52,7 @@ while True:
         break
     except:
         continue
+
 if not tableOne:
     print("Hello " + firstName + ' ' + lastName)
 else:
@@ -60,26 +60,24 @@ else:
 cursor.fetchall()
 while True:
     filmName = input("Please enter a film name:")
-    tableTwo = cursor.execute(("""SELECT *
+    cursor.execute(("""SELECT title
                                         FROM film 
                                         WHERE title=%s
-            """), (filmName,))
+    """), (filmName,))
+    tableTwo=cursor.fetchall()
     if not tableTwo:
         continue
-    filmID = cursor.execute(("""SELECT film.film_id
+    cursor.execute(("""SELECT film.film_id
                                 FROM film 
                                 WHERE title=%s
     """), (filmName,))
+    filmID=cursor.fetchall()
     if len(tableTwo)==1:
         rating = input("Please enter a rating for the film:")
         try:
             cursor.execute('INSERT INTO rating (film_id,reviewer_id,rating) VALUES (%s, %s,%s)',
-                       (filmID,idInput, rating))
+                    (filmID.film_id, idInput, rating))
             cnx.commit()
             break
         except:
             continue
-    cursor.fetchall()
-
-
-
